1b49fd83e52cfbdd016431cf017807cd0cbb0571
Ráth Boglárka <rath.boglarka@jedlik.helyi>
2020-01-09 09:16:36 +0100

    netlify link name change

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    <b-navbar toggleable="lg" type="dark" variant="primary">
+    <b-navbar id="navbar" toggleable="lg" type="dark" variant="primary">
       <b-navbar-brand href="#">Kerítés feladat</b-navbar-brand>
       <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
       <b-collapse id="nav-collapse" is-nav>
@@ -8,39 +8,62 @@
           <b-nav-item href="kerites.txt">kerites.txt</b-nav-item>
           <b-nav-item href="Kerítés_fel.pdf">Feladat.pdf</b-nav-item>
           <b-nav-item href="Kerítés_jav.pdf">Javítási.pdf</b-nav-item>
-          <b-nav-item href="https://github.com/nitslaszlo/ErettsegiKeritesTsVueJs">GitHub</b-nav-item>
-          <b-nav-item href="https://github.com/nitslaszlo/JedlikVueJsStarter">SDK</b-nav-item>
+          <b-nav-item href="https://github.com/Weeboga/ErettsegiKeritesVueJS">GitHub</b-nav-item>
+          <b-nav-item href="">Netlify</b-nav-item>
         </b-navbar-nav>
       </b-collapse>
     </b-navbar>
-    <TxtReader label="Forrás (kerites.txt):" placeholder="Bedobó mező" @load="txtSorai = $event" />
-    <div v-if="mutat" id="megoldas">
-      <p>2. feladat<br />Az eladott telkek száma: {{ telkek.length }}</p>
-      <p>
-        3. feladat<br />A {{ utolsoTelek.oldal }} oldalon adták el az utolsó telket<br />
-        Az utolsó telek házszáma: {{ utolsoTelek.hazSzama }}
-      </p>
-      <p>4. feladat<br />A szomszédossal egyezik a kerítés színe: {{ ugyanOlyanSzinuKerites }}</p>
-      <p>
-        5. feladat<br />Adjon meg egy házszámot!
-        <input v-model="hazszamInputStr" type="number" min="1" max="117" /><br />
-        A kerírés színe / állapota: {{ keritesSzineAllapota }}<br />
-        Egy lehetséges festési szín: {{ lehetsegesFestesiSzin }}
-      </p>
+    <TxtReader
+      label="Forrásfájl (kerites.txt):"
+      placeholder="Kérem töltse be a forrásfájlt!"
+      @load="txtSorai = $event"
+    />
+    <div v-if="mutat" id="megoldas" style="float:left">
+      <b-tabs id="tabok" content-class="mt-4" align="center">
+        <b-tab title="2.feladat">
+          <p>Az eladott telkek száma: {{ telkek.length }}</p>
+        </b-tab>
+        <b-tab title="3.feladat">
+          <p>
+            A {{ utolsoTelek.oldal }} oldalon adták el az utolsó telket<br />
+            Az utolsó telek házszáma: {{ utolsoTelek.hazSzama }}
+          </p>
+        </b-tab>
+        <b-tab title="4.feladat">
+          <p>A szomszédossal egyezik a kerítés színe: {{ ugyanOlyanSzinuKerites }}</p>
+        </b-tab>
+        <b-tab title="5.feladat">
+          <p>
+            Adjon meg egy házszámot!
+            <input v-model="hazszamInputStr" type="number" min="1" max="117" /><br />
+            A kerírés színe / állapota: {{ keritesSzineAllapota }}<br />
+            Egy lehetséges festési szín: {{ lehetsegesFestesiSzin }}
+          </p>
+        </b-tab>
+      </b-tabs>
     </div>
-    <p v-if="mutat">
-      <TxtWriter title="utcakep.txt állomány mentése" :content="utcakep" filename="utcakep.txt" />
-    </p>
-    <!-- Megoldás DIV -->
-
-    <!-- Nem a feladat része : -->
-    <div v-if="mutat" id="egyebek">
-      <pre>
+    <div v-if="mutat" id="egyebek" style="float:right" class="">
+      <div class="overflow-auto">
+        <pre>
 utcakep.txt fájl:
-{{ utcakep }}
+{{ utcakep.trim() }}
+      </pre
+        >
+      </div>
+    </div>
+    <div v-if="mutat" id="egyebek" style="float:right">
+      <div class="overflow-auto">
+        <pre>
 kerites.txt fájl:
-{{ txtSorai }}
-      </pre>
+{{ txtSorai.trim() }}
+      </pre
+        >
+      </div>
+    </div>
+    <div class="position-relative d-inline">
+      <b-button v-if="mutat" pill variant="success" class="savebutton mx-auto">
+        <TxtWriter title="utcakep.txt állomány mentése" :content="utcakep" filename="utcakep.txt" />
+      </b-button>
     </div>
   </div>
 </template>
@@ -147,15 +170,37 @@ export default class App extends Vue {
 </script>
 
 <style>
-#app {
-  font-family: Courier;
+pre {
+  color: white;
 }
 
 #megoldas {
-  background-color: lightgrey;
+  background-color: #c84d5f;
+  color: #ddc23f;
   padding: 0px 10px;
   border-radius: 10px;
-  max-width: 600px;
+  max-width: 550px;
+  height: 200px;
+  text-align: center;
+  margin: 20px;
+}
+
+#egyebek {
+  background-color: #c84d5f;
+  color: #ddc23f;
+  padding: 0px 10px;
+  border-radius: 10px;
+  max-width: 550px;
+  height: 200px;
+  text-align: center;
+  margin: 20px;
+}
+b-tab {
+  margin-top: 155px;
+}
+
+b-tab title {
+  color: #ddc23f;
 }
 
 a {
@@ -164,11 +209,17 @@ a {
 }
 
 input[type="number"] {
-  background-color: lightgray;
+  background-color: white;
+  border: 2px gold solid;
+  border-radius: 10px;
+  text-align: center;
 }
 
-pre {
-  font-size: 1.1em;
-  margin: 0;
+.savebutton {
+  background-color: #c84d5f;
+  border: 2px solid red;
+  color: gold;
+  position: center;
+  display: flex;
 }
 </style>
